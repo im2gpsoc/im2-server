834d7b6a4709d59552aea3cbc70dadcbfee56063
coyo1 <coyo1@224828-WIN7.npfit.nhs.uk>
2015-05-08 12:53:53 +0100

    Use DSTU2

--- src/main/java/uk/gov/hscic/gpsoc/im2/IM2Server.java
+++ src/main/java/uk/gov/hscic/gpsoc/im2/IM2Server.java
@@ -4,18 +4,14 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 
-import ca.uhn.fhir.context.FhirVersionEnum;
-
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.FhirVersionEnum;
 import ca.uhn.fhir.jpa.dao.IFhirSystemDao;
-import ca.uhn.fhir.jpa.provider.JpaConformanceProviderDstu1;
 import ca.uhn.fhir.jpa.provider.JpaConformanceProviderDstu2;
-import ca.uhn.fhir.jpa.provider.JpaSystemProviderDstu1;
 import ca.uhn.fhir.jpa.provider.JpaSystemProviderDstu2;
-import ca.uhn.fhir.model.api.IResource;
 import ca.uhn.fhir.model.dstu2.resource.Bundle;
 import ca.uhn.fhir.narrative.DefaultThymeleafNarrativeGenerator;
 import ca.uhn.fhir.rest.server.ETagSupportEnum;
@@ -29,102 +25,49 @@ public class IM2Server extends RestfulServer {
 
 	private static final long serialVersionUID = 1L;
 
-	private WebApplicationContext myAppCtx;
+	private WebApplicationContext appCtx;
 
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void initialize() throws ServletException {
 		super.initialize();
 
-		/* 
-		 * We want to support FHIR DSTU2 format. This means that the server
-		 * will use the DSTU2 bundle format and other DSTU2 encoding changes.
-		 *
-		 * If you want to use DSTU1 instead, change the following line, and change the 2 occurrences of dstu2 in web.xml to dstu1
-		 */
-		FhirVersionEnum fhirVersion = FhirVersionEnum.DSTU2;
-		setFhirContext(new FhirContext(fhirVersion));
+		/* Use DSTU2 */
+		setFhirContext(new FhirContext(FhirVersionEnum.DSTU2));
 
-		// Get the spring context from the web container (it's declared in web.xml)
-		myAppCtx = ContextLoaderListener.getCurrentWebApplicationContext();
+		appCtx = ContextLoaderListener.getCurrentWebApplicationContext();
 
-		/* 
-		 * The hapi-fhir-server-resourceproviders-dev.xml file is a spring configuration
-		 * file which is automatically generated as a part of hapi-fhir-jpaserver-base and
-		 * contains bean definitions for a resource provider for each resource type
-		 */
-		String resourceProviderBeanName = "myResourceProvidersDstu" + (fhirVersion == FhirVersionEnum.DSTU1 ? "1" : "2");
-		List<IResourceProvider> beans = myAppCtx.getBean(resourceProviderBeanName, List.class);
+		/* Set resource providers - DSTU2 */ 
+		List<IResourceProvider> beans = appCtx.getBean("myResourceProvidersDstu2", List.class);
 		setResourceProviders(beans);
 		
-		/* 
-		 * The system provider implements non-resource-type methods, such as
-		 * transaction, and global history.
-		 */
-		Object systemProvider;
-		if (fhirVersion == FhirVersionEnum.DSTU1) {
-			systemProvider = myAppCtx.getBean("mySystemProviderDstu1", JpaSystemProviderDstu1.class);
-		} else {
-			systemProvider = myAppCtx.getBean("mySystemProviderDstu2", JpaSystemProviderDstu2.class);
-		}
-		setPlainProviders(systemProvider);
+		/* Set system provider (for non-resource-type methods, such as transaction) - DSTU2 */
+		setPlainProviders(appCtx.getBean("mySystemProviderDstu2", JpaSystemProviderDstu2.class));
 
-		/*
-		 * The conformance provider exports the supported resources, search parameters, etc for
-		 * this server. The JPA version adds resource counts to the exported statement, so it
-		 * is a nice addition.
-		 */
-		if (fhirVersion == FhirVersionEnum.DSTU1) {
-			IFhirSystemDao<List<IResource>> systemDao = myAppCtx.getBean("mySystemDaoDstu1", IFhirSystemDao.class);
-			JpaConformanceProviderDstu1 confProvider = new JpaConformanceProviderDstu1(this, systemDao);
-			confProvider.setImplementationDescription("GPSoC IM2 Server");
-			setServerConformanceProvider(confProvider);
-		} else {
-			IFhirSystemDao<Bundle> systemDao = myAppCtx.getBean("mySystemDaoDstu2", IFhirSystemDao.class);
-			JpaConformanceProviderDstu2 confProvider = new JpaConformanceProviderDstu2(this, systemDao);
-			confProvider.setImplementationDescription("GPSoC IM2 Server");
-			setServerConformanceProvider(confProvider);
-		}
+		/* Set conformance provider - DSTU2 */
+		IFhirSystemDao<Bundle> systemDao = appCtx.getBean("mySystemDaoDstu2", IFhirSystemDao.class);
+		JpaConformanceProviderDstu2 confProvider = new JpaConformanceProviderDstu2(this, systemDao);
+		confProvider.setImplementationDescription("GPSoC IM2 Server");
+		setServerConformanceProvider(confProvider);
 
-		/*
-		 * Enable ETag Support (this is already the default)
-		 */
+		/* Enable ETag Support */
 		setETagSupport(ETagSupportEnum.ENABLED);
 
-		/*
-		 * This server tries to dynamically generate narratives
-		 */
-		FhirContext ctx = getFhirContext();
-		ctx.setNarrativeGenerator(new DefaultThymeleafNarrativeGenerator());
+		/* Dynamically generate narratives */
+		getFhirContext().setNarrativeGenerator(new DefaultThymeleafNarrativeGenerator());
 
-		/*
-		 * This tells the server to use "browser friendly" MIME types if it 
-		 * detects that the request is coming from a browser, in the hopes that the 
-		 * browser won't just treat the content as a binary payload and try 
-		 * to download it (which is what generally happens if you load a 
-		 * FHIR URL in a browser). 
-		 * 
-		 * This means that the server isn't technically complying with the 
-		 * FHIR specification for direct browser requests, but this mode
-		 * is very helpful for testing and troubleshooting since it means 
-		 * you can look at FHIR URLs directly in a browser.  
-		 */
+		/* Use "browser friendly" MIME types if the request is from a browser (not FHIR-compliant) */
 		setUseBrowserFriendlyContentTypes(true);
 
-		/*
-		 * Default to XML and pretty printing
-		 */
-		setDefaultPrettyPrint(true);
-		setDefaultResponseEncoding(EncodingEnum.JSON);
+		/* Default to XML */
+		setDefaultResponseEncoding(EncodingEnum.XML);
 
-		/*
-		 * This is a simple paging strategy that keeps the last 10 searches in memory
-		 */
+		setDefaultPrettyPrint(true);
+
+		/* Keep last 10 searches in memory */
 		setPagingProvider(new FifoMemoryPagingProvider(10));
 
-		/*
-		 * Do some fancy logging to create a nice access log that has details about each incoming request.
-		 */
+		/* Configure logging */
 		LoggingInterceptor loggingInterceptor = new LoggingInterceptor();
 		loggingInterceptor.setLoggerName("fhir.access");
 		loggingInterceptor.setMessageFormat("Path[${servletPath}] Operation[${operationType} ${idOrResourceName}] UA[${requestHeader.user-agent}] Params[${requestParameters}]");
